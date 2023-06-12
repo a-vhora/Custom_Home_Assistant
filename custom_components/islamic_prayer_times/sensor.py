64a9ce88911e8248b181adeddb6d6681f039629d
Aadil Vhora <75792623+a-vhora@users.noreply.github.com>
2023-06-12 19:48:36 +1000

    Update sensor.py

--- custom_components/islamic_prayer_times/sensor.py
+++ custom_components/islamic_prayer_times/sensor.py
@@ -5,7 +5,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 import homeassistant.util.dt as dt_util
-import pytz
 
 from .const import DATA_UPDATED, DOMAIN, PRAYER_TIMES_ICON, SENSOR_TYPES
 
@@ -51,7 +50,7 @@ class IslamicPrayerTimeSensor(SensorEntity):
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        return self.client.prayer_times_info.get(self.sensor_type).astimezone(pytz.timezone('Australia/Sydney'))
+        return self.client.prayer_times_info.get(self.sensor_type)
         )
 
     async def async_added_to_hass(self) -> None:
